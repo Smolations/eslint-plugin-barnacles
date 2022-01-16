f98b4f2332bff9661765952fb7809ff6b14b83db
Chris Smola <callmesmola@gmail.com>
2022-01-16 15:26:53 -0700

    fills out test file a bit

---
+++ tests/fixtures/object-curly-newline.js
@@ -0,0 +1,13 @@
+const invalid = `
+const foo = {
+ one: 1, two: 2, three: 3
+};
+`;
+
+const valid = `
+const foo = {
+ one: 1,
+ two: 2,
+ three: 3
+};
+`;
--- tests/lib/rules/object-curly-newline.js
+++ tests/lib/rules/object-curly-newline.js
@@ -2,14 +2,14 @@
  * @fileoverview make sure that properties are always on their own line when original rule is triggered
  * @author Smolations
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/object-curly-newline"),
-  RuleTester = require("eslint").RuleTester;
+const rule = require('../../../lib/rules/object-curly-newline'),
+  RuleTester = require('eslint').RuleTester;
 
 
 //------------------------------------------------------------------------------
@@ -17,15 +17,31 @@ const rule = require("../../../lib/rules/object-curly-newline"),
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
-ruleTester.run("object-curly-newline", rule, {
+ruleTester.run('object-curly-newline', rule, {
   valid: [
     // give me some code that won't trigger a warning
+    {
+      code: `
+        const foo = {
+          one: 1,
+          two: 2,
+          three: 3
+        };
+      `,
+    }
   ],
 
   invalid: [
     {
-      code: "no",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      code: `
+        const foo = {
+          one: 1, two: 2, three: 3
+        };
+      `,
+      errors: [{
+        message: 'Properties must be on new line',
+        type: 'object-curly-newline-type',
+      }],
     },
   ],
 });
