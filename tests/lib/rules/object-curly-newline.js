a019a7f0e33bd4414a5b3a2a91dc7a41edc4d182
Chris Smola <callmesmola@gmail.com>
2022-01-17 12:27:12 -0700

    all tests passing

--- tests/lib/rules/object-curly-newline.js
+++ tests/lib/rules/object-curly-newline.js
@@ -51,12 +51,12 @@ ruleTester.run('object-curly-newline', rule, {
   invalid: [
     {
       code: `const foo = { one: 1, two: 2, three: 3 };`,
-      errors: [{ messageId: 'requireMultiline' }],
+      errors: [{ messageId: 'requireMultilineInit' }],
       // type: '...', // references node type
     },
     {
       code: `const { one, two, three } = foo;`,
-      errors: [{ messageId: 'requireMultiline' }],
+      errors: [{ messageId: 'requireMultilineDestructure' }],
     },
     // {
     //   code: `function ({ one, two, three }) {}`,
