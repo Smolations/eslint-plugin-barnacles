092fdf49b2614dd46b721afc268d5226d9b662b5
Chris Smola <callmesmola@gmail.com>
2022-01-17 14:02:09 -0700

    updates project lint config, lints, and adds new helper function for rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
-"use strict";
+'use strict';
 
 module.exports = {
   root: true,
   extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
+    'eslint:recommended',
+    'plugin:eslint-plugin/recommended',
+    'plugin:node/recommended',
   ],
   env: {
     node: true,
   },
   parserOptions: {
-    // parser: '@babel/eslint-parser',
-    ecmaVersion: 6,
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 8,
+  },
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'function-paren-newline': ['error', 'multiline'],
+    'quotes': ['error', 'single'],
   },
   overrides: [
     {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
+      files: ['tests/**/*.js'],
+      env: { jest: true },
     },
   ],
 };
